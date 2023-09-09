bc991fa817a75f7a204e1197b33abc2c61f96a7b
Ye Wang <ye_wang2@brown.edu>
2023-09-09 18:39:35 -0400

    Introduce the UIUX Portfolio

---
+++ personas/images/img_resize.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import os
+
+# Credit: https://stackoverflow.com/questions/44370469/python-image-resizing-keep-proportion-add-white-background
+def resize(image_pil, width, height):
+    '''
+    Resize PIL image keeping ratio and using white background.
+    '''
+    ratio_w = width / image_pil.width
+    ratio_h = height / image_pil.height
+    if ratio_w < ratio_h:
+        # It must be fixed by width
+        resize_width = width
+        resize_height = round(ratio_w * image_pil.height)
+    else:
+        # Fixed by height
+        resize_width = round(ratio_h * image_pil.width)
+        resize_height = height
+    image_resize = image_pil.resize((resize_width, resize_height), Image.ANTIALIAS)
+    background = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+    offset = (round((width - resize_width) / 2), round((height - resize_height) / 2))
+    background.paste(image_resize, offset)
+    return background.convert('RGB')
+
+
+for file in os.listdir("."):
+    if file.startswith("sb"):
+        resized = resize(Image.open(file), 1200, 900)
+        resized.save("sb_resized" + file[2:])
